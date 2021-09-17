6bf66ce6a1f6549cc164ea8eacf9912d84d62be1
Eric Duwe <ericduwe@gmail.com>
2021-09-17 16:31:11 -0500

    fixed routes in server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = 3001;
 const notes = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 const fs = require('fs');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const app = express();
 
@@ -31,21 +32,7 @@ app.post('/api/notes', (req, res) => {
       note_id: uuid(),
         };
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedNotes = JSON.parse(data);
-
-            parsedNotes.push(newNote)
-
-            fs.writeFile('./db/db.json',
-                JSON.stringify(parsedNotes, null, 4),
-                (writeErr) =>
-                    writeErr ? console.error(writeErr) : console.info("Successfully updated notes")
-            );
-        }
-    })
+    readAndAppend(newNote, './db/db.json');
 
     const response = {
         status: 'success',
@@ -59,6 +46,12 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.get('/api/notes', (req, res) => {
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
 app.get('/notes', (req, res) => {
  // Sending notes.html to the client
     res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -69,11 +62,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
   });
 
-app.get('/api/notes', (req, res) => {
-    // Log our request to the terminal
-    res.status(200).json(`${req.method} request received to get notes`)
-    console.info(`${req.method} request received to get notes`)
-});
+
 
 
 
